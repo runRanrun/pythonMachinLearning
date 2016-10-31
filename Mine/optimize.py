9ad972332206dcd307aceb03bae0167e8d194f07
runRanrun <1120102033@bit.edu.cn>
2016-11-01 00:17:24 +0800

    update

--- Mine/optimize.py
+++ Mine/optimize.py
@@ -8,6 +8,17 @@ people = [('Seymour','BOS'),
     ('Walt','MIA'),
     ('Buddy','ORD'),
     ('Les','OMA')]
+destination='LGA'
+flights={}
+for line in file('schedule.txt'):
+    origin,dest,depart,arrive,price=line.strip().split(',')
+    flights.setdefault((origin,dest),[])
+    flights[(origin,dest)].append(depart,arrive,int(price))
+#时间计算函数
+def getminutes(t):
+    x = time.strftime(t,'%H:%M')
+    return x[3]*60+x[4]
+def schedulecost(sol):
 
 
 
