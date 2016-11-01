87f4e21090acad38262f0d4894f450c7ac4ee3aa
runRanrun <1120102033@bit.edu.cn>
2016-11-01 22:42:13 +0800

    update

--- Mine/optimize.py
+++ Mine/optimize.py
@@ -17,9 +17,20 @@ for line in file('schedule.txt'):
 #时间计算函数
 def getminutes(t):
     x = time.strftime(t,'%H:%M')
-    return x[3]*60+x[4]
+    return x[3]*60+x [4]
 def schedulecost(sol):
+    totalprice = 0
+    latestarrival = 0
+    earliestdep = 24*60
+    for d in range(len(sol)/2):
+        origin = people[d][1]
+        outbound=flights[(origin,destination)][int(sol[2*d])]
+        returnf=flights[(destination,origin)][int(sol[2*d+1])]
 
+        totalprice+=outbound[2]
+        totalprice+=returnf[2]
+
+        
 
 
 
