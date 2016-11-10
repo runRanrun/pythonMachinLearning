113aa0f36b483382f9c165eb4457b217d4a1374c
runRanrun <306483777@qq.com>
2016-11-10 19:03:06 +0800

    Merge branch 'windows' into Mac

--- Mine/optimize.py
+++ Mine/optimize.py
@@ -8,7 +8,41 @@ people = [('Seymour','BOS'),
     ('Walt','MIA'),
     ('Buddy','ORD'),
     ('Les','OMA')]
+destination='LGA'
+flights={}
+for line in file('schedule.txt'):
+    origin,dest,depart,arrive,price=line.strip().split(',')
+    flights.setdefault((origin,dest),[])
+    flights[(origin,dest)].append(depart,arrive,int(price))
+#时间计算函数
+def getminutes(t):
+    x = time.strftime(t,'%H:%M')
+    return x[3]*60+x [4]
+def schedulecost(sol):
+    totalprice = 0
+    latestarrival = 0
+    earliestdep = 24*60
+    for d in range(len(sol)/2):
+        origin = people[d][1]
+        outbound=flights[(origin,destination)][int(sol[2*d])]
+        returnf=flights[(destination,origin)][int(sol[2*d+1])]
 
+        totalprice+=outbound[2]
+        totalprice+=returnf[2]
+
+        if latestarrival<getminutes(outbound[1]):
+            latestarrival=getminutes(outbound[1])
+        if earliestdep>getminutes(returnf[0]):
+            earliestdep=getminutes(returnf[0])
+    totalwait=0
+    for d in range(len(sol)/2):
+        origin=people[d][1]
+        outbound=flights[(origin,destination)][int(sol[2*d])]
+        returnf=flights[(destination,origin)][int(sol[2*d+1])]
+        totalwait+=latestarrival-getminutes(outbound[1])
+        totalwait+=getminutes(returnf[0])-earliestdep
+        if latestarrival<earliestdep:totalwait+=50
+        return totalprice+totalwait
 
 
 
