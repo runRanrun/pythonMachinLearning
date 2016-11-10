5bda5b6c6e9f8d5819f25bf1b2b3537fcb36396d
runRanrun <1120102033@bit.edu.cn>
2016-11-10 18:59:58 +0800

    update

--- Mine/optimize.py
+++ Mine/optimize.py
@@ -59,4 +59,15 @@ def schedulecost(sol):
 def hillclimb(domain,costf):
     sol=[random.randint(domain[i][0],domain[i][1]) for i in range(len(domain))]
 
+    while 1:
+        neighbors=[]
+        for j in range(len(domain)):
+            if sol[j]>domain[j][0]:
+                neighbors.append(sol[0:j]+[sol[j]-1]+sol[j+1:])
+            if sol[j]<domain[j][1]:
+                neighbors.append(sol[0:j]+[sol[j]+1]+sol[j+1:])
+        current=costf(sol)
+        best=current
+        for j in range(len(neighbors)):
+            cost=costf(neighbors[j])
 
